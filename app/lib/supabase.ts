888c4ae93e391ed739df6d24e3626419dbb07aba
sachin <sachin@sachins-Air.lan>
2026-05-27 00:27:17 +0530

    fix supabase key error

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -4,8 +4,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export const supabaseAdmin = createClient(
-  supabaseUrl,
-  process.env.SUPABASE_SERVICE_KEY!
-);
+export const supabaseAdmin = supabase;
